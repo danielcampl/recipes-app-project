2260fe7ef8d4a31184b57049ce6fd619bc8d3ba6
Daniel Campelo <danielcampelo159@gmail.com>
2022-10-19 15:02:24 -0300

    Foods header css done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { useHistory } from "react-router-dom";
 
-import profileIcon from '../images/profileIcon.svg';
-import searchIcon from '../images/searchIcon.svg';
-import SearchBar from './SearchBar';
+import profileIcon from "../images/profileIcon.svg";
+import searchIcon from "../images/searchIcon.svg";
+import SearchBar from "./SearchBar";
 
 function Header(props) {
   const [searchBar, setSearchBar] = useState(false);
   const history = useHistory();
 
   function userRedirectPage() {
-    history.push('/profile');
+    history.push("/profile");
   }
 
   function handleEnableBar() {
@@ -20,16 +20,29 @@ function Header(props) {
 
   const { enableSearch, title } = props;
   return (
-    <div>
-      <h1 data-testid="page-title">{ title }</h1>
-      <button type="button" onClick={ userRedirectPage } className="icon-button">
-        <img src={ profileIcon } alt="profileIcon" data-testid="profile-top-btn" />
+    <div className="header">
+      <h1 data-testid="page-title">{title}</h1>
+      <div className="header-btn">
+      <button type="button" onClick={userRedirectPage} className="footer-btn">
+        <img
+          src={profileIcon}
+          alt="profileIcon"
+          data-testid="profile-top-btn"
+          className="footer-img"
+        />
       </button>
-      { enableSearch && (
-        <button type="button" onClick={ handleEnableBar } className="icon-button">
-          <img src={ searchIcon } alt="searchIcon" data-testid="search-top-btn" />
-        </button>)}
-      {searchBar && (<SearchBar />)}
+      {enableSearch && (
+        <button type="button" onClick={handleEnableBar} className="footer-btn">
+          <img
+            src={searchIcon}
+            alt="searchIcon"
+            data-testid="search-top-btn"
+            className="footer-img"
+          />
+        </button>
+      )}
+      </div>
+      {searchBar && <SearchBar />}
     </div>
   );
 }
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -4,6 +4,7 @@ import Header from '../components/Header';
 import AllRecipes from '../components/Recipes';
 import CategoriasRecipes from '../components/CategoriesRecipes';
 import Footer from '../components/Footer';
+import '../css/Foods.css';
 
 function Foods() {
   return (
